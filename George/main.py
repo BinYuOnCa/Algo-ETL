2c93a4978f8414da5d880221725afc29270fcc4b
george <zhoutian4@gmail.com>
2021-01-18 07:03:35 -0500

    Smashed no data handling bugs when getting data

--- George/main.py
+++ George/main.py
@@ -1,7 +1,6 @@
 from datetime import datetime
-import pandas as pd
 
-import load_candles
+import utils.load_candles as load_candles
 import utils.api_timer as api_timer
 import utils.db_func as db_func
 import utils.sql_func as sql_func
@@ -12,63 +11,82 @@ from utils.msg_service import send_email
 try:
     start_time = datetime.today()
     timer = api_timer.api_timer()
+    ticker = ""
     ticker_success = 0
     error_num = 0
     error_flag = 0
     candle_freq_list = ["D", 1]
     ticker_no_data = ""
+    ticker_error = ""
+    db_conn = db_func.DB_Conn()
+    conn = db_conn.create_connection()
     for candle_freq in candle_freq_list:
         if candle_freq == "D":
             table_name = "us_equity_finn_daily"
         elif candle_freq == 1:
             table_name = "us_equity_finn_1m"
         ticker_no_data = ticker_no_data + "\n\n" + str(candle_freq) + ": \n"
-        db_conn = db_func.DB_Conn()
-        conn = db_conn.create_connection()
+        ticker_error = ticker_error + "\n\n" + str(candle_freq) + ": \n"
         ticker_to_update = sql_func.get_ticker_to_update_df(conn)
         # ticker_to_update = pd.DataFrame(pd.Series(['AAPL', "AMZN", "bbb", "thdh"]), columns=["ticker"])
         ticker_count = len(ticker_to_update)
         for index, row in ticker_to_update.iterrows():
-            # ticker = row.ticker
+            ticker = row.ticker
             execution_result = load_candles.load_candles(row.ticker, table_name, candle_freq, timer, conn)
-            if execution_result != "Success":
+            if execution_result == "Success":
+                ticker_success += 1
+            elif execution_result == "no data":
                 ticker_no_data = ticker_no_data + str(row.ticker) + ","
             else:
-                ticker_success += 1
+                ticker_error = ticker_error + str(row.ticker) + ","
+
     if ticker_no_data.count(",") != 0:
         error_flag = 1
         ticker_no_data = ticker_no_data[:-1]
         error_num = error_num + ticker_no_data.count(",")
+    if ticker_error.count(",") != 0:
+        error_flag = 1
+        ticker_error = ticker_error[:-1]
+        error_num = error_num + ticker_error.count(",")
 except Exception as e:
-    print(e)
-    pass
+    try:
+        error_sql = "insert into error_rec (error_time, error_info) values ('" \
+                    + str(datetime.today()).replace("'", "''") + \
+                    "', 'Error when getting data with ticker [" \
+                    + str(ticker) + "]: " + str(e).replace("'", "''") + "')"
+        with conn.cursor() as cursor:
+            cursor.execute(error_sql)
+    except Exception as e2:
+        print('"', datetime.today(),
+              '" , "Error when adding error message to database: ' + str(e2) + '", "'
+              + 'Original Error when getting data with ticker \'', str(ticker), '\': ', e, '"')
 
 finally:
     if error_flag == 0:
        send_sms("Algo-Trading executed correctly, Started at: " + str(start_time) + "\n Finished at: "
-                + str(datetime.today()) + "\ntickers updated: " + str(ticker_success))
+                + str(datetime.today()) + "\ntickers processed: " + str(ticker_success))
        send_email("Algo-Trading executed correctly", "Started at: " + str(start_time) + "\n Finished at: "
-                  + str(datetime.today()) + "\ntickers updated: "
+                  + str(datetime.today()) + "\ntickers processed: "
                   + str(ticker_success)+ "\n Algo trading executed successfully")
     else:
         if execution_result == "Success":
             send_email("Algo-Trading executed finished with " + str(error_num) + " error(s)!",
                        "Algo-Trading executed finished with " + str(error_num)
                        + " error(s)! \nStarted at: " + str(start_time) + "\n Finished at: "
-                       + str(datetime.today()) + "\ntickers updated: " + str(ticker_success)
+                       + str(datetime.today()) + "\ntickers processed: " + str(ticker_success)
                        + "\n tickers list cannot update: " + str(ticker_no_data))
 
             send_sms("Algo-Trading execution finished with " + str(error_num)
                      + " error(s), \nStarted at: " + str(start_time) + "\n Finished at: "
-                    + str(datetime.today()) + "\ntickers updated: " + str(ticker_success))
+                    + str(datetime.today()) + "\ntickers proceessed: " + str(ticker_success))
         else:
             send_email("Algo-Trading executed finished with " + str(error_num)
                        + " error(s)!", "Algo-Trading executed finished with " + str(error_num)
                        + " error(s)! \nStarted at: " + str(start_time) + "\n Finished at: "
-                       + str(datetime.today()) + "\ntickers updated: " + str(ticker_success)
+                       + str(datetime.today()) + "\ntickers processed: " + str(ticker_success)
                        + "\n tickers list cannot update: " + str(ticker_no_data)
                        + 'Last Error detail: "' + str(execution_result))
             send_sms("Algo-Trading execution finished with " + str(error_num)
                      + " error(s), \nStarted at: " + str(start_time) + "\n Finished at: "
-                     + str(datetime.today()) + "\ntickers updated: " + str(ticker_success)
+                     + str(datetime.today()) + "\ntickers processed: " + str(ticker_success)
                      + "last error detail " + str(execution_result))
--- George/utils/load_candles.py
+++ George/utils/load_candles.py
@@ -24,9 +24,11 @@ def load_candles(ticker, table_name, candle_freq, timer, _conn=conn):
     :return:
     """
     try:
+        isempty = 1
         month_step = 120 if candle_freq == "D" else 1
         last_timestamp = sql_func.get_last_timestamp(ticker, table_name, conn)
         if last_timestamp == None:
+            isfirstload = True
             last_close_price = None
             if candle_freq == 1:
                 start_timestamp = ctt.convert_datetime_timestamp(
@@ -35,34 +37,49 @@ def load_candles(ticker, table_name, candle_freq, timer, _conn=conn):
                 start_timestamp = ctt.convert_datetime_timestamp(
                 ctt.get_past_date(_date=datetime.today(), years=25))  # 25 years data
         else:
+            isfirstload = False
             start_timestamp = last_timestamp
             last_close_price = sql_func.get_symbol_close_price(ticker, table_name)
         stop_timestamp = ctt.convert_datetime_timestamp(datetime.today())
-        month_list = ctt.date_by_month_list(
-            ctt.convert_timestamp_datetime(start_timestamp),
-            ctt.convert_timestamp_datetime(stop_timestamp))
-        while month_list.next_timestamp_list(month=month_step) is not None:
-            timer.api_timer_handler()
-            stock_candle = finn_func.get_candles_df(
-                ticker, candle_freq, month_list.timestamp_list[0], month_list.timestamp_list[1])
-            if stock_candle is not None:
-                sql_func.insert_df_to_db(stock_candle.drop(stock_candle.index[0]), table_name)
-                if candle_freq == "D":
-                    db_last_close_price = last_close_price.iloc[0, 0]
-                    api_last_close_price = stock_candle.iloc[0, ]["close_price"]
-                    issplit = False if api_last_close_price == db_last_close_price else True
-                    sql_func.insert_df_to_db(stock_candle.drop(stock_candle.index[0]), table_name)
-                    if issplit is True:
-                        timer.api_timer_handler()
-                        split_df = finn_func.get_split_df(
-                            ticker,
-                            ctt.convert_timestamp_datetime(month_list.timestamp_list[0]),
-                            ctt.convert_timestamp_datetime(month_list.timestamp_list[1]))
-                        if split_df is not None:
-                            sql_func.insert_df_to_db_iter(split_df)
-        return "Success"
+        if start_timestamp + 86400 >= stop_timestamp and candle_freq == "D":
+            month_list = ctt.date_by_month_list(
+                ctt.convert_timestamp_datetime(start_timestamp),
+                ctt.convert_timestamp_datetime(stop_timestamp))
+            while month_list.next_timestamp_list(month=month_step) is not None:
+                timer.api_timer_handler()
+                stock_candle = finn_func.get_candles_df(
+                    ticker, candle_freq, month_list.timestamp_list[0], month_list.timestamp_list[1])
+                if stock_candle is not None:
+                    isempty = isempty and False
+                    if isfirstload:
+                        sql_func.insert_df_to_db(stock_candle, table_name)
+                        isfirstload = False
+                    else:
+                        sql_func.insert_df_to_db(stock_candle.drop(stock_candle.index[0]), table_name)
+                    if candle_freq == "D":
+                        db_last_close_price = last_close_price.iloc[0, 0]
+                        api_last_close_price = stock_candle.iloc[0, ]["close_price"]
+                        issplit = False if api_last_close_price == db_last_close_price else True
+                        sql_func.insert_df_to_db(stock_candle.drop(stock_candle.index[0]), table_name)
+                        if issplit is True:
+                            timer.api_timer_handler()
+                            split_df = finn_func.get_split_df(
+                                ticker,
+                                ctt.convert_timestamp_datetime(month_list.timestamp_list[0]),
+                                ctt.convert_timestamp_datetime(month_list.timestamp_list[1]))
+                            if split_df is not None:
+                                sql_func.insert_df_to_db_iter(split_df)
+                else:
+                    isempty = isempty and True
+        if isempty is True:
+            return "no data"
+        elif isempty is False:
+            return "Success"
+        else:
+            return "Error"
     except Exception as e:
         try:
+
             error_sql = "insert into error_rec (error_time, error_info) values ('" \
                         + str(datetime.today()).replace("'", "''") + \
                         "', 'Error when getting data with ticker [" \
