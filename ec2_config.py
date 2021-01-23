b875672b3d53527d72701f651154e4dce9bad8de
yingwei chen <36582571+pchenwei@users.noreply.github.com>
2021-01-23 07:45:26 -0500

    Update ec2_config.py

--- ec2_config.py
+++ ec2_config.py
@@ -11,17 +11,17 @@ portname='5432'
 param_dic={"host":hostname, "database": dbname, "user": username, "password":passwd }
 
 # Finnhub API Key
-finnhub_api_key="bv8dqn748v6rnm7avf60"
-finnhub_sandbox_key="sandbox_bv8dqn748v6rnm7avf6g"
+finnhub_api_key="**************"
+finnhub_sandbox_key="sandbox_*****************"
 
 finnhub_stock_candle_minute=1
 finnhub_stock_candle_daily='D'
 
 # Twilio credential
-twilio_account_sid = 'AC489e96f24732d0a92f43655b1f31fc3a'
-twilio_auth_token =  'fb369213c30ae782dc878cf5a8a45d78'
-twilio_from='+12512202832',
-twilio_to='+16479783581'
+twilio_account_sid = '***************'
+twilio_auth_token =  '*******************'
+twilio_from='+1222222222',
+twilio_to='+16666666666'
 
 # used by sqlalchemy
 db_connection_url=f"postgresql://{username}:{passwd}@{hostname}:{portname}/{dbname}"
