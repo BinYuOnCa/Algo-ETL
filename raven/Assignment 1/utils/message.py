6556aea24c07637c0cdf3a7eda9a154bfceab064
ravenren <75292001+ravenren@users.noreply.github.com>
2021-01-24 15:44:23 -0500

    Update message.py

--- raven/Assignment 1/utils/message.py
+++ raven/Assignment 1/utils/message.py
@@ -8,16 +8,16 @@ except ModuleNotFoundError:
 def Send_sms():
     client = Client(TWILIO_CONFIG["ACCOUNT_SID"],TWILIO_CONFIG["AUTH_TOKEN"])
     print("Twilio connected")
-    # message = client.messages\
-    #     .create(
-    #     body="ETL completed at "+str(dt.datetime.now()),
-    #     from_="+12349013647",to="+16479847888")
+    message = client.messages\
+        .create(
+        body="ETL completed at "+str(dt.datetime.now()),
+        from_="+12349013647",to="+16479847888")
     print("Message sent")
 
 def error_sms():
     client = Client(TWILIO_CONFIG["ACCOUNT_SID"],TWILIO_CONFIG["AUTH_TOKEN"])
-    # message = client.messages \
-    #     .create(
-    #     body="Error happen at " + str(dt.datetime.now()),
-    #     from_="+12349013647", to="+16479847888")
+    message = client.messages \
+        .create(
+        body="Error happen at " + str(dt.datetime.now()),
+        from_="+12349013647", to="+16479847888")
     print("Message sent")
